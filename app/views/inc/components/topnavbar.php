471d50a485e30801e09c402f80653c08b1e68d7a
Nethmi Athukorala <nethmihao2001@gmail.com>
2023-09-25 00:39:25 +0530

    Added the signupform

--- app/views/inc/components/topnavbar.php
+++ app/views/inc/components/topnavbar.php
@@ -1,7 +1,7 @@
 <div class="topnav">
   <a class="active" href="#home">Home</a>
-  <a href="#news">Login</a>
-  <a href="#contact">Register</a>
+  <a href="#signin">Login</a>
+  <a href="#signup">Register</a>
   <!-- <a href="#about">Logout</a> -->
   <input type="text" placeholder="Search in ecotrade">
  
--- app/views/users/v_register.php
+++ app/views/users/v_register.php
@@ -2,5 +2,43 @@
     <!-- Top NAVIGATION -->
     <?php require APPROOT . '/views/inc/components/topnavbar.php';?>
 
-    <h1>Sign Up</h1>
+
+
+    <div class="form-container">
+        <div class="form-header">
+        <center><h1>Sign Up</h1></center>
+        <p><b>Welcome to EcoTrade! Please sign up to continue.</b></p>
+        </div>
+        <form action="" method="POST">
+            <!-- username -->
+            <div class="form-input-title">Username</div>
+            <input type="text" name="username" placeholder="Username" id="username" class="username" >
+            <span class="form-invalid">invalid username</span>
+
+            <!-- email -->
+            <div class="form-input-title">Email</div>
+            <input type="text" name="email" placeholder="Email" id="email" class="email" >
+            <span class="form-invalid"></span>
+
+            <!-- contact number -->
+            <div class="form-input-title">Contact Number</div>
+            <input type="text" name="number" placeholder="Contact Number" id="number" class="number" >
+            <span class="form-invalid"></span>
+
+            <!-- password -->
+            <div class="form-input-title">Password</div>
+            <input type="password" name="password" placeholder="Password" id="password" class="password" >
+            <span class="form-invalid"></span>
+
+            <!-- confirm password -->
+            <div class="form-input-title">Confirm Password</div>
+            <input type="password" name="confirm_password" placeholder="Confirm Password" id="confirm_password" class="confirm_password" >
+            <span class="form-invalid"></span>
+
+            <!-- submit button -->
+            <br><br>
+            <input type="submit" value="Sign Up" class="form-btn">
+        </form>
+    </div>
+
 <?php require APPROOT.'/views/inc/footer.php'; ?>
